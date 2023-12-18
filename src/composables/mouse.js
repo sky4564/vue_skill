de7e1ec02a750848ee5089fd55c852116c8e6a64
김병민 <kbm5553@gnifrix.com>
2023-12-18 13:33:04 +0900

    mouse.js 컴포저블로 구현

---
+++ src/composables/mouse.js
@@ -0,0 +1,21 @@
+import { ref, onMounted, onUnmounted } from 'vue'
+
+export function useMouse () {
+  const x = ref(0)
+  const y = ref(0)
+
+  function update (event) {
+    console.log('mouse js event')
+    console.log(event)
+    x.value = event.pageX
+    y.value = event.pageY
+  }
+
+  onMounted(() => {
+    window.addEventListener('mousemove', update)
+  })
+  onUnmounted(() => {
+    window.removeEventListener('mousemove', update)
+  })
+  return { x, y }
+}
---
+++ src/views/TestView.vue
@@ -0,0 +1,11 @@
+<template>
+  마우스 위치: {{ x }}, {{ y }}
+</template>
+
+<script setup>
+import { useMouse } from '../composables/mouse.js'
+
+const { x, y } = useMouse()
+</script>
+
+<style></style>
